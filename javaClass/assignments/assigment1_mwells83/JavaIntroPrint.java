49fbeed578947a4a762fcc82632ee856d79057a8
MichaelWells <mwell21@uic.edu>
2019-06-02 20:49:10 -0500

    HW 1 last tweaks

---
+++ javaClass/assignments/assigment1_mwells83/JavaIntroPrint.java
@@ -0,0 +1,16 @@
+//     Michael Wells
+//    EN.605.201.83.SU19
+//    Assignment 1
+//    06/02/19
+//
+
+public class JavaIntroPrint
+{
+  public static void main( String [] args )
+  {
+      System.out.println("    J    A   V     V    A");
+      System.out.println("    J   A A   V   V    A A");
+      System.out.println("J   J  AAAAA   V V    AAAAA");
+      System.out.println(" J J  A     A   V    A     A");
+  }
+}
