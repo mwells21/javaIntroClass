51e2e0549055ff5f9e03ca0aacb0a156f37518aa
MichaelWells <mwell21@uic.edu>
2019-06-16 20:59:56 -0500

    some comments and screen shots for assignment 3

--- javaClass/assignments/assignment3_mwells83/Problem2.java
+++ javaClass/assignments/assignment3_mwells83/Problem2.java
@@ -17,40 +17,67 @@ public class Problem2
   {
     Scanner input = new Scanner( System.in );
 
-    int randomNumber = 0;
-    int maxNumber = 0;
-    int maxGuess = 0;
-    int guess = 0;
-    int i = 0;
-    int time = 1;
-    int restart = 1;
+
+
+    /*
+      -Declare Variables 
+      -Get max number
+      -Get max number of guesses
+      -Create Random Number
+      -Receive Guesses
+      -Output response
+      -Ask the user to play again
+    */
+
+
+    int randomNumber = 0; // decalre random number
+    int maxNumber = 0;    // decalre maxium possible random number
+    int maxGuess = 0;     // decalre maxium possible guesses
+    int guess = 0;        // decalre user guess
+    int i = 0;            // decalre iterator for guesses
+    int time = 1;         // decalre iterator for playing the game
+    int restart = 1;      // declare variable for restart option
+
+
 
     while(time == 1){
       System.out.println("GUESS THE SECRET NUMBER");
       System.out.println("\n");
+
+      //Get max number
       System.out.print("Enter the maxium possible number:");
       maxNumber = input.nextInt();
+
+      //Get max number of guesses
       System.out.print("Enter the maxium number of guesses:");
       maxGuess = input.nextInt();
-
+      //Create Random Number
       randomNumber = (int) (maxNumber * Math.random() ) + 1;
-      System.out.println(randomNumber);
 
+      // Receive Guess
+      // Iterate until you have run out of guesses
       for(i = 1;i <= maxGuess;i++){
         System.out.print("Enter Guess number "+ i +":");
         guess = input.nextInt();
 
+        // Answer was correct
         if(guess == randomNumber){
           System.out.println("YOU ARE CORRECT");
           i = 11;
         }
+        //  Answer Was too low
         if(guess < randomNumber){
           System.out.println("Too Low");
         }
+        //  Answer Was too high
         if(guess > randomNumber){
           System.out.println("Too High");
         }
       }
+
+
+
+      // Asking the user if they want to play again
 
       System.out.println("\n");
       System.out.println("Would you like to play again? Yes(1) No(0)");
