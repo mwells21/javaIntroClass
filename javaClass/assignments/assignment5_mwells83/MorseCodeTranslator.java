de88491911bd0b7ed07194b4b040e22d8d66bee7
mwells21 <mwells21@uic.edu>
2019-06-28 21:01:26 -0400

    assignment 5 start

---
+++ javaClass/assignments/assignment5_mwells83/MorseCodeTranslator.java
@@ -0,0 +1,58 @@
+//    Michael Wells
+//    EN.605.201.83.SU19
+//    Assignment 5
+//    06/30/19
+//
+
+
+import java.util.Scanner;
+
+public class MorseCodeTranslator
+{
+  public static void main( String [] args )
+  {
+    translator();
+
+
+//String inputString = input.nextLine();
+
+
+  }
+
+
+  public static void translator()
+  {
+    Scanner input = new Scanner(System.in);
+
+    int response = 0;
+
+    System.out.println( "-----------MORSE CODE TRANSLATOR-----------");
+    System.out.println( "___________________________________________");
+    System.out.println( "To translate Morse Code --> English enter 1");
+    System.out.println( "To translate English --> Morse Code enter 2");
+
+    response = input.nextInt();
+
+    if(response == 1 ){
+      System.out.println("_______________________");
+      System.out.println("Morse Code --> English\n");
+      System.out.print("Enter Morse Code:");
+      String inputString = input.nextLine();
+      System.out.println(inputString);
+
+    }
+
+    if(response == 2 ){
+      System.out.println("_______________________");
+      System.out.println("English --> Morse Code\n");
+      System.out.print("Enter English:");
+      String inputString = input.nextLine();
+      System.out.println(inputString);
+    }
+
+
+  }
+
+
+
+}
