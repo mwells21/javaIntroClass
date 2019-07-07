25f9859bcc9701e23d615ecd166f33926ed69b50
mwells21 <mwells21@uic.edu>
2019-07-07 12:17:06 -0400

    minor changes

--- javaClass/assignments/assignment6_mwells83/EmployeeModel.java
+++ javaClass/assignments/assignment6_mwells83/EmployeeModel.java
@@ -22,10 +22,11 @@ public class EmployeeModel
     Address addy1 = new Address("Curtis Rd","Northville","MI",48168);
     //System.out.println(addy1.getAddressString());
 
-
+    Employee[] employeeArray = new Employee[10];
 
     Employee emp1 = new Employee(1,n1,addy1,d1);
-    System.out.println(emp1.getEmployeeSummary());
+    employeeArray[0] = emp1;
+    System.out.println(employeeArray[0].getEmployeeSummary());
 
   }
 }
