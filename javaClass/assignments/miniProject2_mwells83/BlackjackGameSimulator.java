f74dd380c6fdf9424510beb61971b2b9ca189708
MichaelWells <mwell21@uic.edu>
2019-07-29 23:13:37 -0500

    start of mini project

---
+++ javaClass/assignments/miniProject2_mwells83/BlackjackGameSimulator.java
@@ -0,0 +1,15 @@
+//    Michael Wells
+//    EN.605.201.83.SU19
+//    Mini Project 2
+//    07/28/19
+//
+
+
+public class BlackjackGameSimulator
+{
+  public static void main( String [] args )
+  {
+    System.out.println("HELLO WORLD");
+
+  }
+}
---
+++ javaClass/assignments/miniProject2_mwells83/Player.java
@@ -0,0 +1,38 @@
+public class Player
+{
+  private int money;
+  private int wager;
+
+
+  public Player(int money, int wager)
+  {
+    this.money = money;
+    this.wager = wager;
+
+  }
+
+  public int getMoney()
+  {
+    return money;
+  }
+
+  public int getWager()
+  {
+    return wager;
+  }
+
+  public setMoney(int money)
+  {
+    this.money = money;
+  }
+
+  public setWager(int wager)
+  {
+    this.wager = wager;
+
+  }
+
+
+
+
+}
