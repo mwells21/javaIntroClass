699021cc57d53c26b19d1ca5abb8a1dd8a3a024f
MichaelWells <mwell21@uic.edu>
2019-08-03 00:43:06 -0500

    almost done with module 10

--- javaClass/assignments/assignment10_mwells83/IncomePropertyReport.java
+++ javaClass/assignments/assignment10_mwells83/IncomePropertyReport.java
@@ -17,12 +17,15 @@ public class IncomePropertyReport
 
     final String fileName = "/home/mwe11s/github/javaIntroClass/javaClass/assignments/assignment10_mwells83/test.dat";
     int fileLength = 13486;
+    int states = 56;
+
 
     // arrays for the summary outputs
-    String[] stateOut = new String[56];
-    int[] popOut = new int[56];
-    int[] childPopOut = new int[56];
-    int[] povOut = new int[56];
+    String[] stateOut = new String[states];
+    int[] popOut = new int[states];
+    int[] childPopOut = new int[states];
+    int[] povOut = new int[states];
+    float[] povRatioOut = new float[states];
 
     // arrays to store data
     String[] stateArray = new String[fileLength];
@@ -35,43 +38,18 @@ public class IncomePropertyReport
     {
       in = new DataInputStream( new BufferedInputStream( new FileInputStream(fileName)));
 
-      //String state = "";
 
       // iterator
       int i = 0;
       while( in.available() > 0)
       {
-        //for( int j = 0; j < 1; j++ ){
-        //  state += in.readChar();
-        //}
-
-
-
+        // Reconstruct Data
         stateArray[i] = in.readUTF();
         populationArray[i] = in.readInt();
         childPopulationArray[i] = in.readInt();
         povertyArray[i] = in.readInt();
 
 
-
-
-        /*
-
-        String state = in.readUTF();
-        System.out.println("State:" + state);
-
-        int pop = 0;
-        pop = in.readInt();
-        System.out.println("Pop:" + pop);
-
-        int childPop = 0;
-        childPop = in.readInt();
-        System.out.println("Child Pop:"+ childPop);
-
-        int pov = 0;
-        pov = in.readInt();
-        System.out.println("Pov:" + pov);
-        */
         i = i + 1;
       }
 
@@ -81,6 +59,65 @@ public class IncomePropertyReport
       {
       System.out.println(e + "Error" );
       }
+
+      String currentState = "01";
+      int totalPop = 0;
+      int totalChild = 0;
+      int totalPov = 0;
+
+      int record = 0;
+      int lastLine = fileLength - 1;
+
+      for(int j = 0; j < fileLength; j++){
+        if(Objects.equals(currentState, stateArray[j])){
+          totalPop = totalPop + populationArray[j];
+          totalChild = totalChild + childPopulationArray[j];
+          totalPov = totalPov + povertyArray[j];
+
+        } else {
+          stateOut[record] = currentState;
+          popOut[record] = totalPop;
+          childPopOut[record] = totalChild;
+          povOut[record] = totalPov;
+          povRatioOut[record] = totalPov / totalChild ;
+
+          System.out.println("State:" + stateOut[record]);
+          System.out.println("Pop:" + popOut[record]);
+          System.out.println("Child:" + childPopOut[record] );
+          System.out.println("Pov:" + povOut[record]);
+          System.out.println("Ratio:" + povRatioOut[record]);
+
+          currentState = stateArray[j];
+          totalPop = populationArray[j];
+          totalChild = childPopulationArray[j];
+          totalPov = povertyArray[j];
+
+
+          record = record + 1 ;
+        }
+        if(j == lastLine){
+          stateOut[record] = currentState;
+          popOut[record] = totalPop;
+          childPopOut[record] = totalChild;
+          povOut[record] = totalPov;
+          povRatioOut[record] = totalPov / totalChild ;
+
+          System.out.println("State:" + stateOut[record]);
+          System.out.println("Pop:" + popOut[record]);
+          System.out.println("Child:" + childPopOut[record] );
+          System.out.println("Pov:" + povOut[record]);
+          System.out.println("Ratio:" + povRatioOut[record]);
+
+        }
+
+      }
+
+
+
+      // Print Summary
+
+
+
 
   }
 }
